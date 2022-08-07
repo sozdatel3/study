4fc0c381afc18e7e55d90f92954e150b94c08804
sozdatel3 <110739584+sozdatel3@users.noreply.github.com>
2022-08-07 20:46:44 +0500

    Update memset.c
    Razobralsya eptit

--- memset.c
+++ memset.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-void memset (void *destination, int c, size_t n) {
-char a[sizeof(size_t)];
-a="(*(char*)destination)";
+void ft_memset (void *destination, int c, size_t n) {
+    char* p=(char*)destination;
 for (int i=0;i<n;i++) {
-    a[i]=c;
+    p[i]=c;
 }
 }
+
+
 
