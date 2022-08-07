5376460362bd05d0dce49d1716ef8ef889a4504c
sozdatel3 <110739584+sozdatel3@users.noreply.github.com>
2022-08-07 21:33:50 +0500

    Add files via upload
    kakayata xyeta, no doljno srabotat

---
+++ memcpy.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void memcpy (void *destination, const void *source, size_t n){
+char* starii=(char*)source;
+char* novii=(char*)destination;
+for(int i=0;i<n;i++){
+    novii[i]=starii[i];
+}
+}
