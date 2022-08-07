68203430b6c66b102099ba7a3f20a193cb0d8e30
sozdatel3 <110739584+sozdatel3@users.noreply.github.com>
2022-08-08 00:28:33 +0500

    Update memove.c

--- memove.c
+++ memove.c
@@ -4,7 +4,7 @@ void * memmove( void * destptr, const void * srcptr, size_t num ){
     char* starii=(char*)destptr;
     char* novii=(char*)srcptr;
     for(int i=0; i<num;i++){
-        novii[num-1-i]=straii[num-1-i];
+        novii[num-1-i]=starii[num-1-i];
 
     }
  return novii;   }
