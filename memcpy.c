d17667bcdf59609cd63a087a85b490570d986fd7
sozdatel3 <110739584+sozdatel3@users.noreply.github.com>
2022-08-07 21:51:10 +0500

    Update memcpy.c

--- memcpy.c
+++ memcpy.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-void memcpy (void *destination, const void *source, size_t n){
+void* memcpy (void *destination, const void *source, size_t n){
 char* starii=(char*)source;
 char* novii=(char*)destination;
 for(int i=0;i<n;i++){
     novii[i]=starii[i];
 }
+   void* p=novii;
+    return p;
+    
 }
