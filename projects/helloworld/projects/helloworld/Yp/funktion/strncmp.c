9697860b4f6766efbf45a60473f9564dc989a520
sozdatel3 <kachan333.333@gmail.com>
2022-08-10 00:25:05 +0500

    modified:   strncmp.c new file:   strncmp.exe

--- projects/helloworld/projects/helloworld/Yp/funktion/strncmp.c
+++ projects/helloworld/projects/helloworld/Yp/funktion/strncmp.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 
-int FT_strncmp( const char * string1, const char * string2, size_t num ){
+int ft_strncmp( const char * string1, const char * string2, size_t num ){
     char* str1=(char*)string1;
     char* str2=(char*)string2;
-    int sravnitel=0;
+    int compaer=0;
     for (int i =0;i<num;i++){
-        if ((str1[i]==0)&&(str2[i]!=0)){
-            sravnitel=-2;
+     if (str1[i]==str2[i]) {
+        continue;     }
+     else{
+        if (str1[i]>str2[i]){
+            compaer=2;
             break;
         }
-        if ((str1[i]!=0)&&(str2[i]==0)){
-            sravnitel=2;
+        if (str1[i]<str2[i]){
+            compaer=-2;
             break;
-        }
-
+            }
     }
-    return sravnitel;
+    }
+    return compaer;
 
 }
 
+int main(){
+  const char b= 'Irik pidr';
+  const char m=  'Irik ne pidr';
+const char * y=&b;
+const char * n=&m;
+int h=ft_strncmp(y,n,6);
+printf("%d",h);
+}
