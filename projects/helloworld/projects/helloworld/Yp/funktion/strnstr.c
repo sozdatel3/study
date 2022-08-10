70774b068f30e092fa118cc2715ae25b1907ae70
sozdatel3 <110739584+sozdatel3@users.noreply.github.com>
2022-08-11 01:22:10 +0500

    Update strnstr.c
    Signed-off-by: sozdatel3 <110739584+sozdatel3@users.noreply.github.com>

--- projects/helloworld/projects/helloworld/Yp/funktion/strnstr.c
+++ projects/helloworld/projects/helloworld/Yp/funktion/strnstr.c
@@ -4,7 +4,7 @@
 char * ft_strnstr (const char * s1, const char * s2, size_t len){
 char* ss1 = (char*)s1;
 char* ss2 = (char*)s2;
-int k = 0;
+int n = 0;
 int size2 = strlen(ss2);
 char* ptr=NULL;
 if (s2[0]==0){
@@ -12,25 +12,25 @@ if (s2[0]==0){
 }
 
 for(int i=0;i<len;i++){
-if (ss1[i]==ss2[k]){
-    k++;
-    if (k==size2-1){
-        ptr=&ss1[i-k];
-        break;
-    }  
-}
-else {if(k!=0) {
-    i=i-1;
-}
-    k=0;
-}
+    for(int k=0;k<size2;k++){
+    if (ss1[i+k]==ss2[k]){
+        n++;
+        if (n==size2-1){
+            ptr=&ss1[i];
+            break;
+        }
+    }
+    else { n=0;}
+    }
+
 }
 return ptr;
 }
 
-const char Y[20]="Yarik lox";
-const char L[5]="lox";
+const char Y[20]="llllox";
+const char L[5]="i";
 int main(){
 ft_strnstr(Y,L,20);
 printf("%p",ft_strnstr(Y,L,20));
+return 0;
 }
